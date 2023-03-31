a3299d0b21525f5b240471ab771a349d841af33c
Yu-Chang YANG <yang.yc.allium@gmail.com>
2023-03-31 19:02:55 +0800

    feat: get screenshot periodically

--- SlideSlicer.py
+++ SlideSlicer.py
@@ -2,6 +2,8 @@
 
 import os
 import sys
+import numpy as np
+from datetime import datetime
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
@@ -53,27 +55,61 @@ class Ui_MainWindow(object):
         self.helpButton.setText(_translate("MainWindow", "?"))
         self.closeButton.setText(_translate("MainWindow", "x"))
 
-def readSettings():
+def readSettings(ui):
     settings = QtCore.QSettings("SlideSlicer_config.ini", QtCore.QSettings.IniFormat)
     helpStr = "*** Edit this file to configure settings and restart when necessary ***"
     settings.setValue("HELP/help", helpStr)
     settings.setValue("ABOUT/name", "SlideSlicer")
     settings.setValue("ABOUT/version", "0.1.0")
     settings.setValue("ABOUT/repository", "https://github.com/Mikumikunisiteageru/SlideSlicer")
-    path = settings.value("OUTPUT/path")
-    if not path or not os.path.isdir(path):
-        path = os.path.join(os.getcwd(), "SlideSlicer_output")
-        if not os.path.isdir(path):
-            os.mkdir(path)
-        settings.setValue("OUTPUT/path", path)
+    ui.path = settings.value("OUTPUT/path")
+    if not ui.path or not os.path.isdir(ui.path):
+        ui.path = os.path.join(os.getcwd(), "SlideSlicer_output")
+        if not os.path.isdir(ui.path):
+            os.mkdir(ui.path)
+        settings.setValue("OUTPUT/path", ui.path)
+    try:
+        string = settings.value("SCREENSHOT/everymillisecond")
+        ui.s = int(string)
+        assert ui.s > 999
+    except:
+        ui.s = 2000
+        settings.setValue("SCREENSHOT/everymillisecond", ui.s)
+
+def screenshot(ui):
+    return ui.screen.grabWindow(0).toImage()
+
+def recordClick(ui):
+    if ui.recording:
+        ui.recordButton.setText("=")
+        ui.recording = False
+    else:
+        ui.recordButton.setText("@")
+        ui.recording = True
+        periodStart(ui)
+
+def periodStart(ui):
+    img = screenshot(ui)
+    img.save(os.path.join(ui.path, datetime.now().strftime("%Y%m%d_%H%M%S.png")))
+    # print(os.path.join(ui.path, datetime.now().strftime("%Y%m%d_%H%M%S.png")))
+    ui.timer.start(ui.s)
+
+def periodRefresh(ui):
+    if ui.recording:
+        periodStart(ui)
 
 if __name__ == '__main__':
-    readSettings()
     app = QtWidgets.QApplication(sys.argv)
     mainWindow = QtWidgets.QMainWindow()
     mainWindow.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.SubWindow)
     ui = Ui_MainWindow()
+    readSettings(ui)
+    ui.screen = QtWidgets.QApplication.primaryScreen()
+    ui.timer = QtCore.QTimer()
+    ui.timer.timeout.connect(lambda: periodRefresh(ui))
+    ui.recording = False
     ui.setupUi(mainWindow)
+    ui.recordButton.clicked.connect(lambda: recordClick(ui))
     ui.helpButton.clicked.connect(lambda: os.system("start SlideSlicer_config.ini"))
     ui.closeButton.clicked.connect(sys.exit)
     mainWindow.show()
